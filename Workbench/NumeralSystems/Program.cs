dd6c2100f6adaab570cc22ab10b6ea2a5a62faa4
BaiGanio <lubenkikov@gmail.com>
2017-11-04 22:48:15 +0200

    Initial project architecture created. Included basic examples for tasks 1, 2 and 3.

---
+++ Workbench/NumeralSystems/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace NumeralSystems
+{
+    /*
+     Преобразувайте числото от десетична в двоична, осмична и шестнайсетична бройна система. 
+     Решенията се приемат само ако сметките са разписани (частно,остатък...).
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            STD1A();
+            STD3A();
+        }
+
+        #region Groups
+
+        private static void STD1A()
+        {
+            // code goes here
+        }
+
+        private static void STD3A()
+        {
+            // code goes here
+        }
+
+        #endregion
+
+        #region Private methods
+
+        #endregion
+
+    }
+}
