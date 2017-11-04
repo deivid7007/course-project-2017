1de423be15e97a3e4f7af330fbc7a46e57521229
BaiGanio <lubenkikov@gmail.com>
2017-11-04 23:01:10 +0200

    Updated task 5.

--- Workbench/Test3/Program.cs
+++ Workbench/Test3/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Test3
 {
+    /*
+        1. Name method like: 
+        _12_0328009_LyubenKikov 
+        or 
+        _7_II_0328009_LyubenKikov
+
+        2. Include it in your group method
+        3. Implementation of the task should be placed in 'Private methods' region
+    */
     class Program
     {
         static void Main(string[] args)
         {
+            STD1A();
+            STD3A();
         }
+
+        #region Groups
+
+        private static void STD1A()
+        {
+            _12_0328009_LyubenKikov();
+            // your code goes here
+        }
+
+        private static void STD3A()
+        {
+            _7_II_0328009_LyubenKikov();
+            // your code goes here
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private static void _12_0328009_LyubenKikov()
+        {
+            Console.WriteLine("Hello STD 1A");
+        }
+
+        private static void _7_II_0328009_LyubenKikov()
+        {
+            Console.WriteLine("Hello STD 3A");
+        }
+
+        #endregion
+
     }
 }
